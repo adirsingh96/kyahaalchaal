c1ad92b0b51e38696e730c6f676e1f51139af43d
adi <adirsingh96@gmail.com>
2024-01-24 23:14:09 +0530

    mail can be stored to db now!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 require('dotenv').config();
 
+const {MongoClient}=require('mongodb');
+const mongoClient=new MongoClient(process.env.MONGODB_URL)
 
 var Imap = require('imap'),
     inspect = require('util').inspect;
@@ -13,7 +15,7 @@ var imap = new Imap({
   port: parseInt(process.env.EMAIL_PORT, 10),
   tls: true
 });
-
+/*
 function openInbox() {
     imap.openBox('INBOX', false, function(err, box) {
       if (err) throw err;
@@ -69,7 +71,112 @@ function openInbox() {
       });
     });
   }
+  */
   
+
+
+  async function fetchAndPrintData() {
+    const uri = process.env.MONGODB_URL; // MongoDB URI
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  
+    try {
+      await client.connect();
+      const database = client.db('kay_haal_chaal'); // Replace with your database name
+      const collection = database.collection('emails'); // Replace with your collection name
+  
+      // Fetch all documents from the collection
+      const documents = await collection.find({}).toArray();
+  
+      // Print each document
+      documents.forEach(doc => {
+        console.log(doc);
+      });
+  
+    } catch (error) {
+      console.error('An error occurred while fetching data:', error);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+
+  function openInbox() {
+    imap.openBox('INBOX', false, async function(err, box) {
+      if (err) throw err;
+  
+      // Connect to MongoDB
+      await mongoClient.connect();
+      const db = mongoClient.db('kay_haal_chaal'); // Replace with your actual db name
+      const collection = db.collection('emails'); // Replace with your actual collection name
+  
+      var messageNumber = box.messages.total; // Get the number of the most recent message
+      var f = imap.seq.fetch(messageNumber + ':' + messageNumber, {
+        bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)', 'TEXT']
+      });
+  
+      f.on('message', function(msg, seqno) {
+        let emailData = {
+          html: '',
+          text: '',
+          date: '',
+          subject: '',
+          from: ''
+        };
+  
+        msg.on('body', function(stream, info) {
+          let buffer = '';
+          stream.on('data', function(chunk) {
+            buffer += chunk.toString('utf8');
+          });
+  
+          stream.once('end', function() {
+            if (info.which === 'HEADER.FIELDS (FROM TO SUBJECT DATE)') {
+              let header = Imap.parseHeader(buffer);
+              if (header.date) {
+                emailData.date = header.date[0];
+              }
+              if (header.subject) {
+                emailData.subject = header.subject[0];
+              }
+              if (header.from) {
+                emailData.from = header.from[0];
+              }
+            } else if (info.which === 'TEXT') {
+              // Assume we want to save the text part of the email as well
+              emailData.text = buffer;
+            }
+          });
+        });
+  
+        msg.once('end', async function() {
+          // Insert email data into MongoDB
+          try {
+            await collection.insertOne(emailData);
+            console.log('Email data inserted into MongoDB');
+            mongoClient.close().then(() => {
+              console.log('MongoDB connection closed.');
+              process.exit(); // Exit the node process if required
+            }).catch((error) => {
+              console.error('Error closing MongoDB connection:', error);
+              process.exit(1); // Exit with an error code
+            });
+
+          } catch (insertError) {
+            console.error('Error inserting email data into MongoDB:', insertError);
+          }
+        });
+      });
+  
+      f.once('error', function(err) {
+        console.log('Fetch error: ' + err);
+      });
+  
+      f.once('end', function() {
+        console.log('Done fetching the most recent message.');
+        imap.end();
+      });
+    });
+  } 
 
 imap.once('ready', function() {
 
@@ -81,3 +188,6 @@ imap.once('ready', function() {
 
    
 imap.connect();
+
+
+fetchAndPrintData();
